0dc9a42174ae76a1678383544244e1c7ac6435f4
Kevin Qiang <butterflybutterpot@gmail.com>
2022-02-11 04:11:05 +0800

    update mumbai json rpc

--- src/connectors/index.js
+++ src/connectors/index.js
@@ -6,7 +6,7 @@ const RPC_URLS = {
   1: 'https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
   4: 'https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
   137: 'https://polygon-rpc.com/',
-  80001: 'https://rpc-mumbai.maticvigil.com/',
+  80001: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`,
 }
 
 export const injected = new InjectedConnector({
