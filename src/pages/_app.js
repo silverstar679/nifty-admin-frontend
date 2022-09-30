0d9153c727430db9a565d7cf49553bf5a14a68df
Kevin <kevchi679@gmail.com>
2022-09-30 15:14:44 +0900

    fix: meta links import

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -35,7 +35,12 @@ const App = (props) => {
             <CacheProvider value={emotionCache}>
               <Head>
                 <title>Nifty Royale Admin</title>
-                <meta name="viewport" content="initial-scale=1, width=device-width" />
+                <meta
+                  name="viewport"
+                  content="width=device-width, initial-scale=1, shrink-to-fit=no"
+                />
+                <link rel="manifest" href="/manifest.json" />
+                <link rel="shortcut icon" href="/favicon.ico" />
               </Head>
               <LocalizationProvider dateAdapter={AdapterDateFns}>
                 <ThemeProvider theme={theme}>
